12ef54766a023fa2172e39fe2eb6e05c1c89c003
Mohammed Ammar Yasir <100485656+MYasir161201@users.noreply.github.com>
2023-05-28 00:38:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,9 +44,9 @@ const Navbar = () => {
         <div className='hidden lg:flex flex-col fixed top-[35%] left-0'>
           <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'><a href='/' className='flex justify-between items-center w-full text-gray-300'>LinkedIn<FaLinkedin size={30} /></a></li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600'><a className='flex justify-between items-center w-full text-gray-300'>GitHub<FaGithub size={30} /></a></li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600'><a className='flex justify-between items-center w-full text-gray-300'>Email<HiOutlineMail size={30} /></a></li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600'><a className='flex justify-between items-center w-full text-gray-300'>GitHub<BsFillPersonLinesFill size={30} /></a></li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600'><a href='/' className='flex justify-between items-center w-full text-gray-300'>GitHub<FaGithub size={30} /></a></li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-green-600'><a href='/' className='flex justify-between items-center w-full text-gray-300'>Email<HiOutlineMail size={30} /></a></li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600'><a href='/' className='flex justify-between items-center w-full text-gray-300'>GitHub<BsFillPersonLinesFill size={30} /></a></li>
           </ul>
         </div>
        
